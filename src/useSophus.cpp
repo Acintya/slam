92ecdb7d95dd91f6802f4d6b2cddbf8589b4de99
ling <lingsheng.hs@gmail.com>
2018-10-14 00:08:53 +0200

    finish useSophus

--- src/useSophus.cpp
+++ src/useSophus.cpp
@@ -23,5 +23,38 @@ int main(int argc, char const *argv[])
     cout << "SO(3) from vector: " << SO3_v << endl;
     cout << "SO(3) from quaternion: " << SO3_q << endl;
 
+    // get lie algebra via exponential map
+    Eigen::Vector3d so3 = SO3_R.log();
+    cout << "so3 = " << so3.transpose() << endl;
+    // hat = inverse-symmetrisic matrix of vector
+    cout << "so3_hat = " << endl << Sophus::SO3::hat(so3) << endl;
+    // vee = inverse-symmetrisic vector of matrix
+    cout << "so3_vee = " << endl << Sophus::SO3::vee(Sophus::SO3::hat(so3)).transpose() << endl;
+
+    // small noise model
+    Eigen::Vector3d update_so3 (1e-4, 0, 0);
+    Sophus::SO3 SO3_updated = Sophus::SO3::exp(update_so3) * SO3_R;
+    cout << "SO3_updated =" << endl;
+    cout << SO3_updated << endl;
+
+    /********SE(3)***********/
+    Eigen::Vector3d t (1, 0, 0); // translation along x axis
+    Sophus::SE3 SE3_Rt(R, t); // generate SE(3) with R, t
+    Sophus::SE3 SE3_qt(q, t);
+    cout << "SE_Rt = " << endl << SE3_Rt << endl; 
+    cout << "SE_qt = " << endl << SE3_qt << endl;
+
+    //se(3)
+    typedef Eigen::Matrix<double, 6, 1> Vector6d;
+    Vector6d se3 = SE3_qt.log();
+    cout << "se(3) = " << endl << se3 << endl;
+
+    cout << "se(3) hat:" << Sophus::SE3::hat(se3) << endl;
+    cout << "se(3) matrix vee" << Sophus::SE3::vee(Sophus::SE3::hat(se3)).transpose() << endl;
+    
+    Vector6d se3_update;
+    se3_update.setZero();
+    Sophus::SE3 SE3_updated = Sophus::SE3::exp(se3_update)*SE3_Rt;
+    cout << "SE_3updated: " << SE3_updated.matrix() << endl;
     return 0;
 }
